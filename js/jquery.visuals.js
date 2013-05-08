20e600c162e90239d718b6e427a450c37498da32
Richard Jansen <humanications@gmail.com>
2013-05-08 22:44:24 +0200

    tweaks\

--- js/jquery.visuals.js
+++ js/jquery.visuals.js
@@ -6,7 +6,6 @@
     $.visuals.prototype.showVisual1 = function() {
         var deferred = new $.Deferred();
         var self = this;
-        console.log('in showVisual1');
         $('div.iframe').append($('<iframe id="webgl-content-1"></iframe>'));
         $('h1.header,div.content,div.img,div.earth,div.body,#fft').hide();
         $('#webgl-content-1').attr('src', '/visuals/1/');
@@ -23,9 +22,7 @@
 
     $.visuals.prototype.stopVisual1 = function() {
         var deferred = new $.Deferred();
-        console.log('in stopVisual1');
         $('div.iframe').fadeOut(2000, function() {
-            console.log('visual1 gestopt');
             deferred.resolve();
         });
         return deferred;
